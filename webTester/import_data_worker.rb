79cd21845fb70beb031c750c3668982f3c701b0e
baldur <baldur.gudbjornsson@gmail.com>
2014-04-14 10:48:16 -0400

    adds logging and debugging and host key for database connection

--- webTester/import_data_worker.rb
+++ webTester/import_data_worker.rb
@@ -2,19 +2,21 @@ require 'sucker_punch/job'
 require 'logger'
 
 require File.join( File.dirname(__FILE__), 'deps')
-CONF = YAML.load_file('settings/settings.yml')
+CONF = YAML.load_file('/etc/on_the_road/settings.yml')
 
 class ImportDataWorker
   include SuckerPunch::Job
 
   def perform(name)
-    logger = ::Logger.new('/var/log/jetty/import_data_worker.log')
-    ActiveRecord::Base.establish_connection(
+    logger = ::Logger.new('/var/log/tomcat7/import_data_worker.log')
+    logger.info("connecting: to sqlite")
+    sqlite_connection = ActiveRecord::Base.establish_connection(
       adapter: 'sqlite3',
       database: "#{CONF[:upload_path]}/#{name}"
     )
     all_data = {}
     ActiveRecord::Base.connection.tables.each do |table|
+      logger.info("table #{table}")
       unless table =~ /android_metadata|log_entries/
         class_name = table.singularize.camelize
         collection = []
@@ -24,15 +26,20 @@ class ImportDataWorker
         all_data[class_name] = collection
       end
     end
+
+    logger.info("trying new connection")
     ActiveRecord::Base.establish_connection(
       :adapter => CONF[:adapter],   # 'mysql2',
       :database => CONF[:database], # "on_the_road",
       :username => CONF[:username], # "test",
-      :password => CONF[:password]  # "test"
+      :password => CONF[:password], # "test"
+      :host => CONF[:host]  # "test"
     )
+
+    logger.info("with connection:")
     all_data.each do |key, value|
       value.each do |stuff|
-        record = key.constantize.new(stuff) 
+        record = key.constantize.new(stuff)
         if record.valid?
           begin
             record.save!
@@ -45,7 +52,7 @@ class ImportDataWorker
         else
           logger.warn("this record is not valid:")
           logger.warn(record.errors.to_s)
-        end 
+        end
       end
     end
   end
