3225e53c3db4b84106ba338c7c5278363646be9d
baldur <baldur.gudbjornsson@gmail.com>
2014-04-23 15:11:31 -0400

    removes unused import

--- src/main/java/com/mapzen/helpers/GeometryHelper.java
+++ src/main/java/com/mapzen/helpers/GeometryHelper.java
@@ -2,8 +2,6 @@ package com.mapzen.helpers;
 
 import android.location.Location;
 
-import static java.lang.Math.toRadians;
-
 public class GeometryHelper {
     public static double getBearing(Location p1, Location p2) {
         return (p1.bearingTo(p2)+360) % 360;
