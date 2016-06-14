2bbdf4fa30ee1c13e1d952664514a28339e87bcf
Sarah Lensing <sarah@mapzen.com>
2016-06-14 13:35:04 -0700

    Support multimodal costing

--- src/main/java/com/mapzen/valhalla/Router.kt
+++ src/main/java/com/mapzen/valhalla/Router.kt
@@ -4,7 +4,8 @@ interface Router {
     enum class Type(private val type: String) {
         WALKING("pedestrian"),
         BIKING("bicycle"),
-        DRIVING("auto");
+        DRIVING("auto"),
+        MULTIMODAL("multimodal");
 
         override fun toString(): String {
             return type
@@ -24,6 +25,7 @@ interface Router {
     fun setWalking(): Router
     fun setDriving(): Router
     fun setBiking(): Router
+    fun setMultimodal(): Router
     fun setLocation(point: DoubleArray): Router
     fun setLocation(point: DoubleArray, heading: Float): Router
     fun setLocation(point: DoubleArray,
