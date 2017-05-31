dbe64d20e7700c20614b2da74200793f3b492712
Sarah Lensing <sarahlensing@gmail.com>
2017-05-31 10:01:51 -0600

    Enable TLS 1.2 on pre-lollipop devices (#116)
    * enable tls 1.2 on pre-lollipop
    
    * update doc

---
+++ library/src/main/java/com/mapzen/http/Tls12SocketFactory.kt
@@ -0,0 +1,74 @@
+package com.mapzen.http
+
+import java.io.IOException
+import java.net.InetAddress
+import java.net.Socket
+
+import javax.net.ssl.SSLSocket
+import javax.net.ssl.SSLSocketFactory
+
+/**
+ * From https://github.com/square/okhttp/issues/2372.
+
+ * Enables TLS v1.2 when creating SSLSockets.
+ *
+ *
+ * For some reason, android supports TLS v1.2 from API 16, but enables it by
+ * default only from API 20.
+ * @link https://developer.android.com/reference/javax/net/ssl/SSLSocket.html
+ * *
+ * @see SSLSocketFactory
+ */
+class Tls12SocketFactory
+/**
+ * Public constructor.
+ * @param base
+ */
+(internal val delegate: SSLSocketFactory) : SSLSocketFactory() {
+
+  override fun getDefaultCipherSuites(): Array<String> {
+    return delegate.defaultCipherSuites
+  }
+
+  override fun getSupportedCipherSuites(): Array<String> {
+    return delegate.supportedCipherSuites
+  }
+
+  @Throws(IOException::class)
+  override fun createSocket(s: Socket, host: String, port: Int, autoClose: Boolean): Socket {
+    return patch(delegate.createSocket(s, host, port, autoClose))
+  }
+
+  @Throws(IOException::class)
+  override fun createSocket(host: String, port: Int): Socket {
+    return patch(delegate.createSocket(host, port))
+  }
+
+  @Throws(IOException::class)
+  override fun createSocket(host: String, port: Int, localHost: InetAddress,
+      localPort: Int): Socket {
+    return patch(delegate.createSocket(host, port, localHost, localPort))
+  }
+
+  @Throws(IOException::class)
+  override fun createSocket(host: InetAddress, port: Int): Socket {
+    return patch(delegate.createSocket(host, port))
+  }
+
+  @Throws(IOException::class)
+  override fun createSocket(address: InetAddress, port: Int, localAddress: InetAddress,
+      localPort: Int): Socket {
+    return patch(delegate.createSocket(address, port, localAddress, localPort))
+  }
+
+  private fun patch(s: Socket): Socket {
+    if (s is SSLSocket) {
+      s.enabledProtocols = TLS_V12_ONLY
+    }
+    return s
+  }
+
+  companion object {
+    private val TLS_V12_ONLY = arrayOf("TLSv1.2")
+  }
+}
